bb9df2b391438061202fbb2c86fb86ce9d033e3f
Emanuel Wille <wille.emanuel@gmail.com>
2019-06-04 16:09:13 +0200

    ServiceWorker & Icons added

--- docs/app.js
+++ docs/app.js
@@ -1,3 +1,8 @@
 import { simonGame } from './webcomponents/simon.js';
 
 customElements.define('simon-game', simonGame);
+
+if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('serviceWorker.js')
+        .catch(console.error);
+}
